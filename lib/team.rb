2201be2015b94466bfbec68a1be515f08c5ee536
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-19 19:20:19 -0600

    Add eliminated?, add_player, players_by_position

--- lib/team.rb
+++ lib/team.rb
@@ -1,7 +1,25 @@
 class Team
-  attr_reader :country
+  attr_accessor :eliminated
+  attr_reader   :country,
+                :players
 
   def initialize(country)
-    @country = country
+    @country    = country
+    @eliminated = false
+    @players    = []
+  end
+
+  def eliminated?
+    eliminated
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def players_by_position(position)
+    players.find_all do |player|
+      player.position == position
+    end
   end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -2,45 +2,60 @@ require './lib/team'
 require './lib/player'
 
 RSpec.describe Team do
-  it 'exists' do
-    team = Team.new("France")
+  context '#initialize' do
+    it 'exists' do
+      team = Team.new("France")
 
-    expect(team).to be_an_instance_of(Team)
+      expect(team).to be_an_instance_of(Team)
+    end
+
+    it 'has attributes' do
+      team = Team.new("France")
+
+      expect(team.country).to eq("France")
+      expect(team.eliminated).to be(false)
+      expect(team.players).to eq([])
+    end
   end
 
-  it 'has attributes' do
-    team = Team.new("France")
+  context '#eliminated?' do
+    it 'verifies if eliminated' do
+      team = Team.new("France")
 
-    expect(team.country).to eq("France")
+      expect(team.eliminated?).to be(false)
+
+      team.eliminated = true
+
+      expect(team.eliminated?).to be(true)
+    end
+  end
+
+  context '#add_player(player)' do
+    it 'adds player' do
+      team = Team.new("France")
+      mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+      pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+
+      expect(team.players).to eq([])
+
+      team.add_player(mbappe)
+      team.add_player(pogba)
+
+      expect(team.players).to eq([mbappe, pogba])
+    end
+  end
+
+  context '#players_by_position(position)' do
+    it 'returns array of players by position' do
+      team = Team.new("France")
+      mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+      pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+
+      team.add_player(mbappe)
+      team.add_player(pogba)
+
+      expect(team.players_by_position("midfielder")).to eq([pogba])
+      expect(team.players_by_position("defender")).to eq([])
+    end
   end
 end
-
-# team.eliminated?
-# # => false
-#
-# team.eliminated = true
-#
-# team.eliminated?
-# # => true
-#
-# team.players
-# # => []
-#
-# mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
-# # => #<Player:0x00007fe0d02bd280...>
-#
-# pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
-# # => #<Player:0x00007fe0d0851138...>
-#
-# team.add_player(mbappe)
-#
-# team.add_player(pogba)
-#
-# team.players
-# # => [#<Player:0x00007fe0d02bd280...>, #<Player:0x00007fe0d0851138...>]
-#
-# team.players_by_position("midfielder")
-# # => [#<Player:0x00007fe0d0851138...>]
-#
-# team.players_by_position("defender")
-# # => []
