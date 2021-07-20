eb1a4c227eeeaa94d24c4affb9d16db198d97de8
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-19 19:52:37 -0600

    Add active_players_by_position

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -6,4 +6,12 @@ class WorldCup
     @year  = year
     @teams = teams
   end
+
+  def active_players_by_position(position)
+    players = []
+    teams.each do |team|
+      players << team.players_by_position(position) unless team.eliminated?
+    end
+    players.flatten
+  end
 end
--- spec/world_cup_spec.rb
+++ spec/world_cup_spec.rb
@@ -45,12 +45,30 @@ RSpec.describe WorldCup do
       expect(world_cup.teams).to eq([france, croatia])
     end
   end
-end
 
-# world_cup.active_players_by_position("midfielder")
-# # => [#<Player:0x00007f936c035eb0...>, #<Player:0x00007f936a3595f8...>]
-#
-# croatia.eliminated = true
-#
-# world_cup.active_players_by_position("midfielder")
-# # => [#<Player:0x00007f936c035eb0...>]
+  context '#active_players_by_position(position)' do
+    it 'returns array of active players by position' do
+      france = Team.new("France")
+      mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+      pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+
+      france.add_player(mbappe)
+      france.add_player(pogba)
+
+      croatia = Team.new("Croatia")
+      modric = Player.new({name: "Luka Modric", position: "midfielder"})
+      vida = Player.new({name: "Domagoj Vida", position: "defender"})
+
+      croatia.add_player(modric)
+      croatia.add_player(vida)
+
+      world_cup = WorldCup.new(2018, [france, croatia])
+
+      expect(world_cup.active_players_by_position("midfielder")).to eq([pogba, modric])
+
+      croatia.eliminated = true
+
+      expect(world_cup.active_players_by_position("midfielder")).to eq([pogba])
+    end
+  end
+end
